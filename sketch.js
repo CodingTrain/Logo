4657c32ae7febfceebbe9e4059ee79a9b4a74dc8
TheTastefulToastie <tastefultoastie@gmail.com>
2018-10-27 00:44:31 +0100

    Implemented repeat
    Implemented repeat as per my suggestion during the livestream:
    
    - Define '[' and ']' as tokens.
    - Consume tokens sequentially.
    - Maintain a stack of token sequences.
    - Record all tokens to the top sequence on the stack.
    - '[' pushes a new sequence to the top of the stack.
    - ']' pops a sequence from the top of the stack (or repeats it).
    
    Repeat is implemented by re-inserting the tokens from the sequence at the top of the stack to the front of the token stream.

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let editor;
 let turtle;
 
 function setup() {
-	createCanvas(200, 200);
+	createCanvas(600, 600);
 	angleMode(DEGREES);
 	background(0);
 	turtle = new Turtle(100, 100, 0);
@@ -16,18 +16,51 @@ function goTurtle() {
 	push();
 	turtle.reset();
 	let code = editor.value();
-	let tokens = code.split(' ');
-	let index = 0;
-	while (index < tokens.length) {
-		let token = tokens[index];
-		if (commands[token]) {
-			if (token.charAt(0) === 'p') {
-				commands[token]();
+	let tokens = code.split(/\b/) // Split on word boundary
+		.map(str => str.trim()) // Trim whitespace
+		.filter(str => str !== ''); // Remove empty strings
+
+	// Default stack item, the entire expression is always at the bottom of the stack
+	let stack = [{
+		iter: 1,
+		tokens: []
+	}];
+
+	while (tokens.length) {
+		token = tokens.shift(); // Consume a token from the stream
+		stack[stack.length - 1].tokens.push(token); // Every token must be recorded to the sequence at the top of the stack
+
+		if (token === ']') { // The end of a sequence (unless it needs repeating)
+			if (--stack[stack.length - 1].iter) { // If we still have repetitions remaining for this sequence
+				tokens = stack[stack.length - 1].tokens.concat(tokens); // Prepend the commands we are repeating to the token stream
+				stack[stack.length - 1].tokens = []; // Clear the sequence at the top of the stack (necessary because repeated tokens are added to the stack as normal and don't want duplicates)
 			} else {
-				commands[token](tokens[++index]);
+				// Prepend the finished sequence to it's parent sequence
+				stack[stack.length - 2].tokens = stack[stack.length - 2].tokens.concat(stack.pop().tokens);
+			}
+		} else if (token === '[') { // The beginning of a sequence
+			const stackTop = stack[stack.length - 1]; // The sequence at the stop of the stack
+
+			const sequence = {
+				iter: 1, // By default a sequence of tokens is executed exactly once
+				tokens: []
+			};
+
+			// If this sequence should be repeated
+			if (stackTop.tokens[stackTop.tokens.length - 3] === 'repeat') {
+				sequence.iter = Number(stackTop.tokens[stackTop.tokens.length - 2]);
+			}
+			stack.push(sequence);
+		} else if (commands.hasOwnProperty(token)) { // If this is a valid command to a turtle
+			let cmd = commands[token];
+			if (token.charAt(0) === 'p') {
+				cmd();
+			} else {
+				token = tokens.shift(); // Consume the token that is the argument to the command
+				stack[stack.length - 1].tokens.push(token); // Every token must be recorded to the sequence at the top of the stack
+				cmd(Number(token));
 			}
 		}
-		index++;
 	}
 	pop();
 }
