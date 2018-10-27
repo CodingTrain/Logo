987167ad5d3b6f54f37effee62b8aa5a79222ebd
Danny Robinson <dannyrobinson@gmx.com>
2018-10-27 04:01:19 +0100

    Regex implementation with nested repeats
    This implements the repeat command by unrolling the loops in the string before parsing, like a text-based macro. Also changed the token parser so that new lines can be used to separate tokens.

--- sketch.js
+++ sketch.js
@@ -11,12 +11,25 @@ function setup() {
 	goTurtle();
 }
 
+function unrollRepeats(oldCode) {
+	function regexRepeater(_, p1, p2) {
+		return (p2 + ' ').repeat(p1);
+	};
+	// this regex finds "repeat (number) [(string)]" where string doesn't contain [ or ]
+	// only the innermost nesting gets unrolled on each pass through
+	const newCode = oldCode.replace(/repeat\s(\d+)\s\[([^\]\[]+)\]/gmi, regexRepeater);
+	// if it hasn't changed any text return it, but if some text has been replaced
+	// call the function again to make sure there isn't another outer level to unroll
+	return oldCode === newCode ? oldCode : unrollRepeats(newCode);
+}
+
 function goTurtle() {
 	background(0);
 	push();
 	turtle.reset();
 	let code = editor.value();
-	let tokens = code.split(' ');
+	code = unrollRepeats(code);
+	let tokens = code.split(/\s/);
 	let index = 0;
 	while (index < tokens.length) {
 		let token = tokens[index];
