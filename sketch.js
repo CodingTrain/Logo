66ec6d136c05c57259aee22951818700ecf08127
Gwerbyn <43727879+Gwerbyn@users.noreply.github.com>
2018-10-27 12:18:06 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let editor;
 let turtle;
+let button;
 
 function setup() {
 	createCanvas(200, 200);
@@ -7,27 +8,60 @@ function setup() {
 	background(0);
 	turtle = new Turtle(100, 100, 0);
 	editor = select('#code');
-	editor.input(goTurtle);
+	button = createButton("submit");
+	button.mousePressed(goTurtle);
 	goTurtle();
+}
+
+function lookForReapeat(val){
+	let found = false;
+	let repeats = [];
+	let working = "";
+	for (let i = 0; i < val.length; i++){
+		if (val[i].includes("[")){
+			found = true;
+			working += val[i] + " ";
+			continue;
+		}
+		if (val[i].includes("]")){
+			working += val[i] + " ";
+			repeats.push(working);
+			working = "";
+			found = false;
+			continue;
+		}
+		if (found){
+			working += val[i] + " ";
+		}
+	
+	}
+	return repeats;
 }
 
 function goTurtle() {
 	background(0);
 	push();
 	turtle.reset();
-	let code = editor.value();
-	let tokens = code.split(' ');
+	let code = editor.value();///["["\s]+/
+	let tokens = code.split(" ");
+	let repeats = lookForReapeat(tokens);
 	let index = 0;
+	let count = 0;
 	while (index < tokens.length) {
 		let token = tokens[index];
 		if (commands[token]) {
 			if (token.charAt(0) === 'p') {
 				commands[token]();
-			} else {
+			} else if (token.charAt(0) === 'r'){
+					commands[token](tokens[++index], repeats[count]);
+					count++;
+				} else {
 				commands[token](tokens[++index]);
 			}
 		}
 		index++;
 	}
+	console.log(count);
+	console.table(repeats);
 	pop();
 }
