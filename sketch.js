0a7656f2c0db7085866e75e88292bf2da554bbe9
TheTastefulToastie <tastefultoastie@gmail.com>
2018-10-28 04:07:18 +0000

    Efficiency!
    Using a more elegent and less wasteful approach.
    Reduced string/array operations.
    No longer modifies input string.

--- sketch.js
+++ sketch.js
@@ -15,50 +15,40 @@ function goTurtle() {
 	background(0);
 	push();
 	turtle.reset();
-	let code = editor.value();
-	let tokens = code.split(/\b|(?=\[|\])/) // Split on word boundary
-		.map(str => str.trim()) // Trim whitespace
-		.filter(str => str !== ''); // Remove empty strings
 
-	// Default stack item, the entire expression is always at the bottom of the stack
-	let stack = [{
-		iter: 1,
-		tokens: []
-	}];
+	// Create a new stream and pass it the Logo code
+	const stream = new Stream(editor.value());
 
-	while (tokens.length) {
-		token = tokens.shift(); // Consume a token from the stream
-		stack[stack.length - 1].tokens.push(token); // Every token must be recorded to the sequence at the top of the stack
+	let stack = []; // Store block scopes
+	repeatCount = 1; // Store the repeat count of the last 'repeat' command encountered
 
-		if (token === ']') { // The end of a sequence (unless it needs repeating)
-			if (--stack[stack.length - 1].iter) { // If we still have repetitions remaining for this sequence
-				tokens = stack[stack.length - 1].tokens.concat(tokens); // Prepend the commands we are repeating to the token stream
-				stack[stack.length - 1].tokens = []; // Clear the sequence at the top of the stack (necessary because repeated tokens are added to the stack as normal and don't want duplicates)
-			} else {
-				// Prepend the finished sequence to it's parent sequence
-				stack[stack.length - 2].tokens = stack[stack.length - 2].tokens.concat(stack.pop().tokens);
+	// Loop until we reach the end of the stream
+  while (!stream.endOfStream()) {
+    // Get the next token
+		const token = stream.readToken();
+		if (token === '[') { // Push a new scope to the stack
+			stack.push({
+				repeatCount,
+				startPosition: stream.getPosition()
+			});
+		} else if (token === ']') { // Jump back to the start of the current scope, or pop it from the stack
+			if (stack.length) { // Let the user type a closing brace first without breaking
+				let stackTop = stack[stack.length - 1];
+				if (--stackTop.repeatCount) { // Decrement the repeat count and check it's not zero
+					stream.seekPosition(stackTop.startPosition);
+				} else { // If repeatCount is zero
+					stack.pop();
+				}
 			}
-		} else if (token === '[') { // The beginning of a sequence
-			const stackTop = stack[stack.length - 1]; // The sequence at the stop of the stack
-
-			const sequence = {
-				iter: 1, // By default a sequence of tokens is executed exactly once
-				tokens: []
-			};
-
-			// If this sequence should be repeated
-			if (stackTop.tokens[stackTop.tokens.length - 3] === 'repeat') {
-				sequence.iter = Number(stackTop.tokens[stackTop.tokens.length - 2]);
-			}
-			stack.push(sequence);
-		} else if (commands.hasOwnProperty(token)) { // If this is a valid command to a turtle
+		} else if (token === 'repeat') {
+			// How many times to repeat the next code block we find
+			repeatCount = stream.readNumber();
+		} else if (commands.hasOwnProperty(token)) { // If this is a valid turtle command
 			let cmd = commands[token];
 			if (token.charAt(0) === 'p') {
 				cmd();
 			} else {
-				token = tokens.shift(); // Consume the token that is the argument to the command
-				stack[stack.length - 1].tokens.push(token); // Every token must be recorded to the sequence at the top of the stack
-				cmd(Number(token));
+				cmd(stream.readNumber());
 			}
 		}
 	}
---
+++ stream.js
@@ -0,0 +1,53 @@
+class Stream {
+  constructor(buffer) {
+		this.buffer = buffer;
+		this.index = 0;
+	}
+
+  getPosition() {
+    return this.index;
+  }
+
+  seekPosition(newPosition) {
+    this.index = newPosition;
+  }
+
+	endOfStream() {
+		return this.index >= this.buffer.length;
+	}
+
+	isDigit(chr) {
+		return chr >= '0' && chr <= '9'
+	}
+
+	isLetter(chr) {
+		return (chr >= 'a' && chr <= 'z') || (chr >= 'A' && chr <= 'Z')
+	}
+
+	isWhitespace(chr) {
+		switch (chr.charCodeAt(0)) {
+			case 10: case 13: case 32: return true;
+			default: return false;
+		}
+	}
+
+	skipWhitespace() {
+		while (this.isWhitespace(this.buffer.charAt(this.index))) this.index++;
+	}
+
+	readToken() {
+		this.skipWhitespace();
+    let chr = this.buffer.charAt(this.index);
+    if (chr === '[' || chr === ']') return ++this.index, chr;
+		let command = '';
+    while (this.isLetter(chr = this.buffer.charAt(this.index))) command += chr, ++this.index;
+		return command;
+	}
+
+	readNumber() {
+		this.skipWhitespace();
+		let command = '', chr;
+    for (let chr; this.isDigit(chr = this.buffer.charAt(this.index)); ++this.index) command += chr;
+		return Number(command);
+	}
+}
