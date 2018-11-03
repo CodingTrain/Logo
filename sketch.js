41813e916cbe11f3b3be88cb2160bb1d425ff896
Andrey A. Ugolnik <andrey@ugolnik.info>
2018-11-03 05:20:38 +0300

    Command parser improved.

--- sketch.js
+++ sketch.js
@@ -2,32 +2,36 @@ let editor;
 let turtle;
 
 function setup() {
-	createCanvas(200, 200);
-	angleMode(DEGREES);
-	background(0);
-	turtle = new Turtle(100, 100, 0);
-	editor = select('#code');
-	editor.input(goTurtle);
-	goTurtle();
+    createCanvas(200, 200);
+    angleMode(DEGREES);
+
+    turtle = new Turtle(100, 100, 0);
+
+    editor = select('#code');
+    editor.input(goTurtle);
+
+    goTurtle();
 }
 
 function goTurtle() {
-	background(0);
-	push();
-	turtle.reset();
-	let code = editor.value();
-	let tokens = code.split(' ');
-	let index = 0;
-	while (index < tokens.length) {
-		let token = tokens[index];
-		if (commands[token]) {
-			if (token.charAt(0) === 'p') {
-				commands[token]();
-			} else {
-				commands[token](tokens[++index]);
-			}
-		}
-		index++;
-	}
-	pop();
+    background(0);
+
+    push();
+    turtle.reset();
+
+    let code = editor.value();
+    let tokens = code.split(' ');
+    let index = 0;
+
+    while (index < tokens.length) {
+        let token = tokens[index++];
+
+        if (commandsOneArg[token]) {
+            commandsOneArg[token](tokens[index++]);
+        } else if (commandsNoArgs[token]) {
+            commandsNoArgs[token]();
+        }
+    }
+
+    pop();
 }
--- turtle.js
+++ turtle.js
@@ -1,51 +1,51 @@
-const commands = {
-  "fd": function (amt) {
-    turtle.forward(amt);
-  },
-  "bd": function (amt) {
-    turtle.forward(-amt);
-  },
-  "rt": function (angle) {
-    turtle.right(angle);
-  },
-  "lt": function (angle) {
-    turtle.right(-angle);
-  },
-  "pu": function () {
-    turtle.pen = false;
-  },
-  "pd": function () {
-    turtle.pen = true;
-  }
+const commandsOneArg = {
+    "fd": function(amt) {
+        turtle.forward(amt);
+    },
+    "bd": function(amt) {
+        turtle.forward(-amt);
+    },
+    "rt": function(angle) {
+        turtle.right(angle);
+    },
+    "lt": function(angle) {
+        turtle.right(-angle);
+    }
+}
+
+const commandsNoArgs = {
+    "pu": function() {
+        turtle.pen = false;
+    },
+    "pd": function() {
+        turtle.pen = true;
+    }
 }
 
 class Turtle {
-  constructor(x, y, angle) {
-    this.x = x;
-    this.y = y;
-    this.dir = angle;
-  }
-
-  reset() {
-    console.log(this.x, this.y, this.dir);
-    translate(this.x, this.y);
-    rotate(this.dir);
-    this.pen = true;
-  }
-
-  forward(amt) {
-    amt = parseInt(amt);
-    if (this.pen) {
-      stroke(255);
-      strokeWeight(2);
-      line(0, 0, amt, 0);
+    constructor(x, y, angle) {
+        this.x = x;
+        this.y = y;
+        this.dir = angle;
     }
-    translate(amt, 0);
-  }
 
-  right(angle) {
-    rotate(angle);
-  }
+    reset() {
+        console.log(this.x, this.y, this.dir);
+        translate(this.x, this.y);
+        rotate(this.dir);
+        this.pen = true;
+    }
 
+    forward(amt) {
+        if (this.pen) {
+            stroke(255);
+            strokeWeight(2);
+            line(0, 0, amt, 0);
+        }
+        translate(amt, 0);
+    }
 
+    right(angle) {
+        rotate(angle);
+    }
 }
