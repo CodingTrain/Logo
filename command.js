737c7ba0956937bd5026820e1eb7d57fcd10f879
Luis Miguel Díaz Abril <EyLuismi@users.noreply.github.com>
2018-11-10 14:45:36 +0100

    Improved parser with more OOP and new examples (#33)
    * Improved Parser with more OOP
    
    Added an example of parameters
    
    * Added setxy losed on merge

--- command.js
+++ command.js
@@ -1,7 +1,191 @@
-class Command {
-  constructor(name, arg) {
+
+const COMMAND_TYPES = {
+  STR: 'STR',
+  INT: 'INT',
+  COMMANDS: 'COMMANDS',
+  PARAMETERS: 'PARAMETERS' // Example
+}
+
+class CommandArg {
+  constructor(name, type) {
     this.name = name;
-    this.arg = arg;
-    this.commands = [];
+    this.type = type;
   }
 }
+
+class Command {
+  constructor(name, args, func) {
+    this.name = name;
+    this.argsTemplate = args;
+    this.func = func;
+  }
+}
+
+class CommandExecutor {
+  constructor(command, values) {
+    this.command = command;
+    this.values = [];
+
+    for (let i = 0; i < values.length; i++) {
+      let value = values[i];
+      let argTemplate = this.command.argsTemplate[i].type;
+
+      switch (argTemplate) {
+        case COMMAND_TYPES.STR:
+          this.values.push(value);
+          break;
+        case COMMAND_TYPES.INT:
+          this.values.push(parseInt(value));
+          break;
+        case COMMAND_TYPES.COMMANDS:
+          this.values.push(
+            new Parser(value).parse()
+          );
+          break;
+        case COMMAND_TYPES.PARAMETERS: // Example
+          this.values.push(
+            value.split(' ')
+          );
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  execute() {
+    this.command.func.apply(this, this.values);
+  }
+}
+
+class CommandLookUp {
+  constructor() {
+    this.commands = [];
+  }
+  add(command) {
+    this.commands.push(command);
+  }
+
+  get(name) {
+    let item = null;
+    let index = 0;
+    while(!item && index < this.commands.length) {
+      if (this.commands[index].name === name)
+        item = this.commands[index];
+
+      index++;
+    }
+
+    return item;
+  }
+}
+
+const commandLookUp = new CommandLookUp();
+
+/**
+ * To add a new command, just need the name, the arguments,
+ * and then the function to execute.
+ */
+
+commandLookUp.add(new Command('fd', [
+  new CommandArg('value', COMMAND_TYPES.INT)
+], (value) => {
+  turtle.forward(value);
+}));
+
+commandLookUp.add(new Command('bd', [
+  new CommandArg('value', COMMAND_TYPES.INT)
+], (value) => {
+  turtle.forward(-value);
+}));
+
+commandLookUp.add(new Command('rt', [
+  new CommandArg('value', COMMAND_TYPES.INT)
+], (value) => {
+  turtle.right(value);
+}));
+
+commandLookUp.add(new Command('lt', [
+  new CommandArg('value', COMMAND_TYPES.INT)
+], (value) => {
+  turtle.right(-value);
+}));
+
+commandLookUp.add(new Command('pu', [], () => {
+  turtle.pen = false;
+}));
+
+commandLookUp.add(new Command('pd', [], () => {
+  turtle.pen = true;
+}));
+
+commandLookUp.add(new Command('setxy', [
+  new CommandArg('x', COMMAND_TYPES.INT),
+  new CommandArg('y', COMMAND_TYPES.INT)
+], (x, y) => {
+  turtle.x = x;
+  turtle.y = y;
+}));
+
+commandLookUp.add(new Command('setx', [
+  new CommandArg('x', COMMAND_TYPES.INT)
+], (x) => {
+  turtle.x = x;
+}));
+
+commandLookUp.add(new Command('sety', [
+  new CommandArg('y', COMMAND_TYPES.INT)
+], (y) => {
+  turtle.y = y;
+}));
+
+commandLookUp.add(new Command('home', [], () => {
+  turtle["home"]();
+}));
+
+commandLookUp.add(new Command('radians', [], () => {
+  angleMode(DEGREES);
+}));
+
+commandLookUp.add(new Command('degrees', [], () => {
+  angleMode(RADIANS);
+}));
+
+commandLookUp.add(new Command('repeat', [
+  new CommandArg('lengthLoop', COMMAND_TYPES.INT),
+  new CommandArg('commands', COMMAND_TYPES.COMMANDS)
+], function (lengthLoop, commands) {
+  for (let i = 0; i < lengthLoop; i++) {
+    for (let cmd of commands) {
+      cmd.execute();
+    }
+  }
+}));
+
+/**
+ * Color, added as example. Given a value, it set the stroke.
+ */
+commandLookUp.add(new Command('color', [
+  new CommandArg('color', COMMAND_TYPES.STR)
+], (color) => {
+  // sanity sake let you use hex without the need for #
+  if (color[0] != "#") {
+    color = "#" + color;
+  }
+
+  turtle.strokeColor = color;
+}));
+
+/**
+ * Added as example of taking [...] as not only commands
+ * but strings you can later process.
+ * This command expects 3 args separated by spaces.
+ */
+commandLookUp.add(new Command('author', [
+  new CommandArg('params', COMMAND_TYPES.PARAMETERS)
+], (params) => {
+  const [author, website, twitter] = params;
+  console.log('This repository has been created by:');
+  console.log(`${author} (@${twitter}) - ${website}`);
+}));
+
--- sketch.js
+++ sketch.js
@@ -25,28 +25,6 @@ function setup() {
   goTurtle();
 }
 
-function execute(commands, repcount) {
-  for (let command of commands) {
-    let name = command.name;
-    let arg = command.arg;
-    if (name === "repeat") {
-      for (let i = 0; i < arg; i++) {
-        execute(command.commands, i);
-      }
-    } else {
-      if (arg == "repcount" && repcount) {
-        commandLookUp[name](repcount);
-      } else {
-        if (arg instanceof Array) {
-          commandLookUp[name](...arg);
-        } else {
-          commandLookUp[name](arg);
-        }
-      }
-    }
-  }
-}
-
 function goTurtle() {
   console.log({startX:startX,startY:startY});
   turtle = new Turtle(startX, startY, 0);
@@ -56,8 +34,9 @@ function goTurtle() {
   let code = editor.value();
   let parser = new Parser(code);
   let commands = parser.parse();
-  console.log(commands);
-  execute(commands);
+  for (let cmd of commands) {
+    cmd.execute();
+  }
   pop();
 }
 
@@ -101,12 +80,12 @@ function createTestDataView(cases) {
 }
 
 function mousePressed() {
-  xOffset = mouseX-startX; 
-  yOffset = mouseY-startX; 
+  xOffset = mouseX-startX;
+  yOffset = mouseY-startX;
 }
 
 function mouseDragged() {
-    startX = mouseX-xOffset; 
-    startY = mouseY-yOffset; 
-    goTurtle();
+  startX = mouseX-xOffset;
+  startY = mouseY-yOffset;
+  goTurtle();
 }
