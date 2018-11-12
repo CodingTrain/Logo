85ed04b0e0c86f18c807fddaf7dd047e48f6a267
anuraghazra <hazru.anurag@gmail.com>
2018-11-12 17:25:17 +0530

    Parser bug fixes, merge #41 had bugs

--- parser.js
+++ parser.js
@@ -78,35 +78,9 @@ class Parser {
   parse() {
     let cmdsExecutors = [];
     while (this.remainingTokens()) {
-      let token = this.nextToken();
-      let cmd = undefined;
+      let actualToken = this.nextToken();
 
-      // testCommand to refactor
-      function testCommand(value, data) { return value.test(data) }
-
-      if (testCommand(movement, token)) {
-        cmd = new Command(token, parseFloat(this.nextToken()));
-      } else if (testCommand(noArgsCalls, token)) {
-        cmd = new Command(token);
-      } else if (testCommand(repeat, token)) {
-        cmd = new Command(token, parseInt(this.nextToken()));
-        let toRepeat = this.getRepeat();
-        let parser = new Parser(toRepeat);
-        cmd.commands = parser.parse();
-      } else if (testCommand(setxy,token)) {
-        cmd = new Command(token);
-        let argX = this.nextToken();
-        let argY = this.nextToken();
-        cmd.arg = [parseFloat(argX), parseFloat(argY)];
-      } else if (testCommand(color, token)) {
-        cmd = new Command(token, this.nextToken());
-      } else if (testCommand(setxySingle,token)) {
-        cmd = new Command(token, parseFloat(this.nextToken()));
-      }
-
-      const actualToken = this.nextToken();
       let cmd = commandLookUp.get(actualToken);
-
       if (cmd) {
         let args = [];
         for (let i = 0; i < cmd.argsTemplate.length; i++) {
